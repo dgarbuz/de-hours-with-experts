96f65368f9260312c715f6a91e7c1fddd6eded36
dgarbuz <8354343+dgarbuz@users.noreply.github.com>
2021-02-24 16:26:04 -0600

    fixing some whitespace

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -3,26 +3,26 @@ import sys
 from itertools import permutations
 
 def main():
-    next_biggest_number(sys.argv[1])
+    next_biggest_number(sys.argv[1])
 
 def next_biggest_number(num):
-    #Turn hopefully valid number into a string
-    num = str(num)
+    #Turn hopefully valid number into a string
+    num = str(num)
 
-    #Get list of all unique permutations of digits in num
-    unqPermList = list(set(permutations(num)))
+    #Get list of all unique permutations of digits in num
+    unqPermList = list(set(permutations(num)))
 
-    #Check all elements of unqPermList to see which ones are larger than num
-    candidates = []
-    for x in unqPermList:
-        candidate = int(''.join(list(x)))
-        if candidate > int(num):
-            candidates.append(candidate)
-    #If none are larger than return -1, otherwise return the minimum of the candiates list
-    if not candidates:
-        return -1
-    else:
-        return min(candidates)
+    #Check all elements of unqPermList to see which ones are larger than num
+    candidates = []
+    for x in unqPermList:
+        candidate = int(''.join(list(x)))
+        if candidate > int(num):
+            candidates.append(candidate)
+            #If none are larger than return -1, otherwise return the minimum of the candiates list
+    if not candidates:
+        return -1
+    else:
+        return min(candidates)
 
 if __name__ == "__main__":
-    main()
+    main()
